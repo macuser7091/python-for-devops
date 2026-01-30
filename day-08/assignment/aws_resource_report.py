6c6d33b5064473b39ffe247de953d626a1039539
macuser7091 <macuser7091@gmail.com>
2026-01-30 22:08:41 +0530

    Modified

--- day-08/assignment/aws_resource_report.py
+++ day-08/assignment/aws_resource_report.py
@@ -6,7 +6,6 @@ import sys
 s3_client = boto3.client("s3")
 responce = s3_client.list_buckets()
 
-# ec2_client = boto3.client("ec2")
 client = boto3.client('ec2')
 
 custom_filter = [{
@@ -15,10 +14,6 @@ custom_filter = [{
     
 response = client.describe_instances(Filters=custom_filter)
 
-# response = client.describe_tags(
-
-# ec2_responce = ec2_client.describe_instances()
-# print(type(ec2_responce))
 
 print(response)
 def s3_bucket():
